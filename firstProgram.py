b21f742f3b81f5bcc007644327803112f3b3e953
Omatta Daniel <briandaniel460@gmail.com>
2021-12-08 14:05:51 +0100

    Updated information
    I also added a piece of information on my internship placement

--- firstProgram.py
+++ firstProgram.py
@@ -2,3 +2,4 @@
 AWS = "This is Amazon's first re/Start program in Lagos, Nigeria"
 print(AWS)
 print("I'll be taking my exam soon")
+print("Hoping to begin a six months internship period after my exam")
